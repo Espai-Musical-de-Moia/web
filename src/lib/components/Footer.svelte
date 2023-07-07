7661c92ee208ce4a5685f80293c53a20a200b55d
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-07 11:56:34 +0200

    create components: footer-imagenFooter

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<div>
+    <img src="../img/logo.png" alt="logo-principal">
+</div>
+
+<div class="contacte">
+    <p>espaimusical.moia@gmail.com</p>
+
+</div>
+
+<div class="aviso">
+
+</div>
+
+
+<div class="proteccion">
+
+</div>
