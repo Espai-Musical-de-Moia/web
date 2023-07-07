106b930471af285dd366efb1fc6d236d1082dc7c
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-07 12:21:35 +0200

    create components: footer-imagenFooter

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,14 +4,18 @@
 
 <div class="contacte">
     <p>espaimusical.moia@gmail.com</p>
-
+    <p>+34 627949433 (Dani)</p>
+    <p>+34 655333709 (Queralt)</p>
+/*RRSS*/
 </div>
 
-<div class="aviso">
-
+<div class="avis">
+    <p>Avís Legal</p>
+    <p>Política de Cookies</p>
+    <p>Política de Privacitad</p>
 </div>
 
-
-<div class="proteccion">
-
+<div class="documentacio">
+    <p>Protecció de Dades</p>
+    <p>Documentació Responsable</p>
 </div>
