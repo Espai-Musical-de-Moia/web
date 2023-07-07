4825d21d675deb994fea584bce8b22b8c8ba4471
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-07 14:16:08 +0200

    styles in footer pair programing with Minerva

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,36 +4,45 @@
 </script>
 
 <footer>
-    <div class="logo.principal">
-        <img src="../src/lib/img/logo.png" alt="logo-principal">
-    </div>
+        <div class="logo">
+            <img src="../src/lib/img/logo.png" alt="logo-principal" widht="119px" height="184px">
+        </div>
+    <section>
+        <div class="contacte">
+            <p>espaimusical.moia@gmail.com</p>
+            <p>+34 627949433 (Dani)</p>
+            <p>+34 655333709 (Queralt)</p>
+            <img src={iconMail} alt="correu">
+            <img src={iconTelephone} alt="telefon">
+        </div>
 
-    <div class="contacte">
-        <p>espaimusical.moia@gmail.com</p>
-        <p>+34 627949433 (Dani)</p>
-        <p>+34 655333709 (Queralt)</p>
-        <img src={iconMail} alt="correu">
-        <img src={iconTelephone} alt="telefon">
-    </div>
+        <div class="avis">
+            <p>Avís Legal</p>
+            <p>Política de Cookies</p>
+            <p>Política de Privacitad</p>
+        </div>
 
-    <div class="avis">
-        <p>Avís Legal</p>
-        <p>Política de Cookies</p>
-        <p>Política de Privacitad</p>
-    </div>
-
-    <div class="documentacio">
-        <p>Protecció de Dades</p>
-        <p>Documentació Responsable</p>
-    </div>
+        <div class="documentacio">
+            <p>Protecció de Dades</p>
+            <p>Documentació Responsable</p>
+        </div>
+    </section>
 </footer>
 
 <style>
     footer {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
         background-color: #07090D;
     }
 
-    p{
+    section {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    p {
         color: #FFFFFF;
     }
 
