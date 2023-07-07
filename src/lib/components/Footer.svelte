1fd0cedfd044f2f4af4d298569e3a7261b9b711e
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-07 12:58:14 +0200

    add styles in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,21 +1,34 @@
-<div>
-    <img src="../img/logo.png" alt="logo-principal">
-</div>
+<footer>
+    <div class="logo.principal">
+        <img src="../img/logo.png" alt="logo-principal">
+    </div>
 
-<div class="contacte">
-    <p>espaimusical.moia@gmail.com</p>
-    <p>+34 627949433 (Dani)</p>
-    <p>+34 655333709 (Queralt)</p>
-/*RRSS*/
-</div>
+    <div class="contacte">
+        <p>espaimusical.moia@gmail.com</p>
+        <p>+34 627949433 (Dani)</p>
+        <p>+34 655333709 (Queralt)</p>
+        /*RRSS*/
+    </div>
 
-<div class="avis">
-    <p>Avís Legal</p>
-    <p>Política de Cookies</p>
-    <p>Política de Privacitad</p>
-</div>
+    <div class="avis">
+        <p>Avís Legal</p>
+        <p>Política de Cookies</p>
+        <p>Política de Privacitad</p>
+    </div>
 
-<div class="documentacio">
-    <p>Protecció de Dades</p>
-    <p>Documentació Responsable</p>
-</div>
+    <div class="documentacio">
+        <p>Protecció de Dades</p>
+        <p>Documentació Responsable</p>
+    </div>
+</footer>
+
+<style>
+    footer {
+        background-color: #07090D;
+    }
+
+    p{
+        color: #FFFFFF;
+    }
+
+</style>
