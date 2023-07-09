14ae7756df666fa306c9cea78f666f7a9d0409a9
Minerva1922 <minervapv83@gmail.com>
2023-07-09 20:55:29 +0200

    subidos iconos redes sociales, estilos hr y height del footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,10 @@
 <script>
     import iconMail from "$lib/img/🦆 icon _mail_.svg"
     import iconTelephone from "$lib/img/🦆 icon _telephone_.svg"
+    import iconInstagram from "$lib/img/icon_intagram.svg"
+    import iconFacebook from "$lib/img/icon_facebook.svg"
+    import iconYouTube from "$lib/img/icon_youtube.svg"
+
 </script>
 
 <footer>
@@ -9,6 +13,11 @@
     </div>
     <hr>
     <section>
+        <div class="reds">
+            <img src={iconInstagram} alt="Instagram">
+            <img src={iconFacebook} alt="facebook">
+            <img src={iconYouTube} alt="youtube">
+        </div>
         <div class="contacte">
             <div class="mail">
                 <img src={iconMail} alt="correu">
@@ -17,7 +26,7 @@
 
             <div class="phone">
                 <img src={iconTelephone} alt="telefon">
-                <p>+34 627949433 (Dani)<br>+34 655333709 (Queralt)</p>
+                <p>+34 627949433 (Dani)<br><br>+34 655333709 (Queralt)</p>
             </div>
         </div>
 
@@ -42,6 +51,7 @@
         box-sizing: border-box;
         background-color: #07090D;
         position: relative;
+        height: 380px;
     }
 
     section {
@@ -59,6 +69,14 @@
     p {
         color: #FFFFFF;
     }
+
+    hr {
+        height: 1px;
+        background-color: rgba(0, 0, 0, 0.3);
+        box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.4);
+        opacity: 0.3;
+    }
+
 
     .contacte {
         display: flex;
