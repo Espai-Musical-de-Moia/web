96d8723030c063ccb12123b5856d523dcd16a723
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-11 00:32:37 +0200

    creating app.css

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -15,38 +15,44 @@
             
         
                 <div class="navbar">
-                                 <img src="../src/lib/img/logo.png" alt="logo-principal" widht="119px" height="184px">
+                                 <img src="../src/lib/img/logo.png" alt="logo-principal" widht="50px" height="114px">
                 <nav>
                         <ul>
                            
                                 <li><a href="/proyecto">Projecte</a></li>
                                 <li><a href="/horario">Horari</a></li>
                                 <li><a href="/espectaculos">Espectacles</a></li>
-                                <li><a href="/calendari">Calendari</a></li>
-                                <Button>Inspecio i Preus</Button>                 
-
+                                <li><a href="/calendari">Calendari</a></li>                                       
+                                  
+                        
                                
                         </ul>
+                                        
                 </nav>
-
         </div>
-        
-        
+        <Button>Inspecio i Preus</Button>
 
 
 </header>
 
 
 <style>
+
+        header{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+        }
         .header-background{
-               
+                           
                 position: absolute;
                 z-index: 1;
         }
 
         .navbar{
                 display: flex;
-                justify-content: space-between;
+                align-items: center;
+                gap: 3em;
                 position: relative;
                 z-index: 2;
         }
@@ -55,18 +61,18 @@
                 display: flex;
                 justify-content: space-between;
                 list-style: none;
-
+                gap: 20px;
         }
-        ul li{
-                margin-right: 20px;
-               
-                
-        } 
+      
 
         ul li a{
 
                 text-decoration: none;
+                color: aliceblue;
+               
         } 
 
-       
+       button {
+        background-color: orangered;
+       }
 </style>
