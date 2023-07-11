2e4084a0af702230319a80861d0f5f4ba13ed839
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-11 12:11:42 +0200

    fix header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,96 +1,87 @@
 <script>
     import Button from "./Button.svelte";
-
-  
 </script>
 
-
-
-
 <header>
-        
-        <div class="header-background">
-                <img src="../src/lib/img/fondo.png" alt="fondo-header">
-        </div>
-            
-        
-                <div class="navbar">
-                        <div class="logo">
-                                 <img src="../src/lib/img/logo.png" alt="logo-principal" widht="100%" height="114px">
-                         </div>
-                <nav>
-                        <div class="nav-links">
-                        <ul>
-                           
-                                <li><a href="/proyecto">Projecte</a></li>
-                                <li><a href="/horario">Horari</a></li>
-                                <li><a href="/espectaculos">Espectacles</a></li>
-                                <li><a href="/calendari">Calendari</a></li> 
-                        </ul> 
-                        </div>
-                        <div class="nav-boton">                                     
-                                <Button>Inspecio i Preus</Button>                              
-                        </div> 
-                        <div class="header-text">
-                        <img src = "../src/lib/img/text-header" alt="text-header">
-                        <
-                       
-                </nav>
-        </div>
-                 
-</header>
+    <div class="header-background">
+        <img src="../src/lib/img/fondo.png" alt="fondo-header">
+    </div>
 
+    <div class="navbar">
+        <div class="logo">
+            <img src="../src/lib/img/logo.png" alt="logo-principal" widht="100%" height="114px">
+        </div>
+        <nav>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="/proyecto">Projecte</a></li>
+                    <li><a href="/horario">Horari</a></li>
+                    <li><a href="/espectaculos">Espectacles</a></li>
+                    <li><a href="/calendari">Calendari</a></li>
+                </ul>
+            </div>
+            <div class="nav-boton">
+                <Button>Inspecio i Preus</Button>
+            </div>
+            <div class="header-text">
+                <img src="../src/lib/img/text-header" alt="text-header">
+            </div>
+        </nav>
+    </div>
+</header>
 
 
 <style>
 
-        header{
-                width: 100%;
-                display: flex;
-                
-                
-        }
-        .header-background{
-                           
-                position: absolute;
-                z-index: 1;
-        }
+    header {
+        width: 100%;
+        display: flex;
 
-        .navbar{
-                display: flex;
-                align-items: center;
-                gap: 26em;
-                
-                position: relative;
-                padding-left: 50px;
-                z-index: 2;
-        }
 
-        ul{
-                display: flex;
-                list-style: none;
-                gap: 20px;
-              
-        }
-      
+    }
 
-        ul li a{
+    .header-background {
 
-                text-decoration: none;
-                color: aliceblue;
-              
-        } 
-        nav{
-                display: flex;
-                flex-direction: row;
-                justify-content: flex-end;
-        } 
-       
-        .nav-links{
-                
-        }
+        position: absolute;
+        z-index: 1;
+    }
 
-       .nav-boton{
+    .navbar {
+        display: flex;
+        align-items: center;
+        gap: 26em;
+
+        position: relative;
+        padding-left: 50px;
+        z-index: 2;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 20px;
+
+    }
+
+
+    ul li a {
+
+        text-decoration: none;
+        color: aliceblue;
+
+    }
+
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+    }
+
+    .nav-links {
+
+    }
+
+    .nav-boton {
         padding-left: 20em;
-       } 
+    }
 </style>
