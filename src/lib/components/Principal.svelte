dd467ef4e171fc9c3c2ea69a8ac364f103e2c869
ikram <adamlujein@gmail.com>
2023-07-11 13:04:25 +0200

    text-principal

---
+++ src/lib/components/Principal.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Principal from "$lib/components/Principal.svelte"
+</script>
+<div class="text-principal">
+    <h2>
+        Volem una comarca rica en cultura on les
+        Arts tinguin un paper central
+    </h2>
+    <h3>
+        Arts tinguin un paper central
+    </h3>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 import Hero from "$lib/components/Hero.svelte";
+import Principal from "$lib/components/Principal.svelte";
 </script>
 
+<Principal/>
 <Hero/>
+
