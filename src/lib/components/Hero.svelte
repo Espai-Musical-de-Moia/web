a3b37a4bfa89e98754814e5e43911d33c4cdec8f
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-11 12:06:58 +0200

    pair programming with Minerva styles Hero Component

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,16 +1,19 @@
 <script>
     import fondo_proyecte from "$lib/img/fondo_proyecte.png"
-    import fondo_texto from "$lib/img/Fondo_texto_projecte.png"
+    import fondo_gris from "$lib/img/Fondo_gris_projecte.png"
 </script>
 <section>
     <div class="fondo_proyecte">
         <img src={fondo_proyecte} alt="fondo_proyecte">
     </div>
 
+    <div class="fondo_gris">
+        <img src={fondo_gris} alt="fondo_proyecte">
+    </div>
+
     <div class="fondo_texto">
-        <img src={fondo_texto} alt="fondo_proyecte">
         <h2>El Nostre Proyecte</h2>
-        <p> L’Espai Musical de Moià (EMM) és una cooperativa sense ànim de lucre que té com a objectiu principal
+        <p>L’Espai Musical de Moià (EMM) és una cooperativa sense ànim de lucre que té com a objectiu principal
             democratitzar la pràctica musical i artística de Moià i el Moianès. La participació dels socis i els seus
             interessos musicals i artístics són el motor de creixement i desenvolupament de l’EMM. És un projecte
             innovador i jove, creat per un grup de pedagogs musicals que sentien la necessitat de promoure l’activitat
@@ -27,14 +30,35 @@
 </section>
 
 <style>
+    section {
+        position: relative;
+        display: inline-block;
+        text-align: center;
+    }
+
     .fondo_proyecte {
         display: flex;
         position: absolute;
+        width: auto;
+    }
+
+    .fondo_gris {
+        display: flex;
+        position: relative;
+        margin-left: 120px;
     }
 
     .fondo_texto {
         display: flex;
-        position: relative;
-
+        position: absolute;
+        flex-wrap: wrap;
+        top: 720px;
+        justify-content: space-evenly;
     }
+
+    p {
+        margin-inline-start: 220px;
+        margin-inline-end: 150px;
+    }
+
 </style>
