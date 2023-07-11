9c6a1c96b34c9147ca86c2ca2ba9a6a92655307d
ikram <adamlujein@gmail.com>
2023-07-11 15:07:58 +0200

    añadir slot

--- src/lib/components/Principal.svelte
+++ src/lib/components/Principal.svelte
@@ -1,12 +1,33 @@
-<script>
-    import Principal from "$lib/components/Principal.svelte"
-</script>
+
 <div class="text-principal">
-    <h2>
+    <h1>
         Volem una comarca rica en cultura on les
+
+    </h1>
+    <h2>
         Arts tinguin un paper central
     </h2>
-    <h3>
-        Arts tinguin un paper central
-    </h3>
 </div>
+
+<style>
+    .text-principal {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        background-color: #f2f2f2;
+        padding: 20px;
+        font-size: 16px;
+    }
+
+    .text-principal h1 {
+        color: blue;
+        font-size: 39px;
+    }
+
+    .text-principal h2 {
+        color: red;
+        font-size: 50px;
+    }
+    </style>
