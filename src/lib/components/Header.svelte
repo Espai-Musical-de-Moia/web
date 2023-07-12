fe225372e4048fb10eca9c91b5586de71518b9d4
Javi <javi@jsrois.net>
2023-07-12 16:06:50 +0200

    Marcar enlace del header cuando se está en una página

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
     import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
     import logo from "$lib/img/logo.png";
 
     function handleButtonClick() {
@@ -16,16 +17,16 @@
             <nav>
                 <div class="nav-links">
                     <ul>
-                        <li>
+                        <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
                             <a href="/">Projecte</a>
                         </li>
-                        <li>
+                        <li aria-current={$page.url.pathname === '/horaris' ? 'page' : undefined}>
                             <a href="/horaris">Horari</a>
                         </li>
-                        <li>
+                        <li aria-current={$page.url.pathname === '/espectacles' ? 'page' : undefined}>
                             <a href="/espectacles">Espectacles</a>
                         </li>
-                        <li>
+                        <li aria-current={$page.url.pathname === '/calendari' ? 'page' : undefined}>
                             <a href="/calendari">Calendari</a>
                         </li>
                         <li>
@@ -92,6 +93,10 @@
         padding: 0.4em;
         border: none;
         color: aliceblue;
+    }
+
+    li[aria-current="page"] {
+        background-color: rgba(128, 128, 128, 0.597);
     }
 
 </style>
