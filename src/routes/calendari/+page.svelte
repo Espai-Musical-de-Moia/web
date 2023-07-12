f99d4e74b43b06fa3f216c794f985ab7079bb6d7
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-12 11:31:21 +0200

    pair programming Minerva-create Calendari

--- src/routes/calendari/+page.svelte
+++ src/routes/calendari/+page.svelte
@@ -1,1 +1,25 @@
+<script>
+    import calendari from "$lib/img/Calendari/Calendari-Juliol.png"
+</script>
+
 <h1>Calendari</h1>
+<div>
+ <img src="{calendari}" alt="calendariJuliol">
+</div>
+
+
+<style>
+ h1 {
+  margin: 30px;
+ }
+
+ div {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+ }
+
+ img {
+  width: 100%;
+ }
+</style>
