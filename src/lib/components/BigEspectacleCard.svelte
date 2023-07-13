c4e33bd9e9cab98f3ada2d27471a54400770b994
ikram <adamlujein@gmail.com>
2023-07-13 14:21:21 +0200

    arreglar 1 article

---
+++ src/lib/components/BigEspectacleCard.svelte
@@ -0,0 +1,36 @@
+<script>
+    import main from "$lib/img/Espectacles/main.png";
+</script>
+
+<article>
+    <div>
+        <h1>L’ORQUESTRA DE CAMBRA DEL MOIANÈS</h1>
+        <p>L’Orquestra de Cambra del Moianès és el conjunt més gran del projecte. Agrupa músics
+            joves i adults de la comarca amb un alt nivell instrumental amb la intenció de preparar
+            repertori simfònic de qualitat. A poc a poc, l'orquestra vol crear i esdevenir un marc musical
+            de referència a la comarca, essent un atractiu pels joves músics moianesos que vulguin
+            estudiar la música de manera semiprofessional o professional, oferint al mateix temps a les
+            audiències de la comarca i de més enllà concerts i espectacles de qualitat, a més de seguir
+            potenciant col·laboracions amb altres artistes del Moianès.</p>
+
+    </div>
+
+    <img src={main} alt="fotoMain">
+</article>
+
+<style>
+    article {
+        width: 100%;
+        padding: 25px;
+        display: flex;
+        justify-content: center;
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        max-width: 20%;
+    }
+    img {
+        max-width: 20%;
+    }
+</style>
---
+++ src/lib/components/Espectacles.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let titulo
+    export let texto
+    export let imagen
+
+
+</script>
+<div class="orquestras">
+    <h1>{titulo}</h1>
+    <p> {texto}</p>
+    <img src={imagen} alt="imagen">
+</div>
+
+<style>
+    p {
+        font-size: 23px;
+        text-align: center;
+
+    }
+
+
+    .orquestras {
+        width: 100%;
+
+
+        display: grid;
+
+        flex-direction: column;
+        grid-gap: 30px;
+
+    }
+
+
+    img {
+
+        width: 100%;
+        height: 250px;
+    }
+
+    h1 {
+
+        width: 100%;
+        color: firebrick;
+        font-weight: bold;
+       text-align: center;
+
+    }
+
+
+</style>
