b025d97b62b0fa0684ce2036505dc28390ec5e2c
NellyKaykay <elelyonexclusive@gmail.com>
2023-07-25 20:08:32 +0200

    creating responsive navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,97 +1,128 @@
 <script>
-    import {goto} from '$app/navigation';
-    import {page} from '$app/stores';
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
     import logo from "$lib/img/header/logo.png";
-    import fondo from "$lib/img/header/fondo.png"
+    import fondo from "$lib/img/header/fondo.png";
 
     let isMobileMenuOpen = false;
 
-  function toggleMobileMenu() {
-    isMobileMenuOpen = !isMobileMenuOpen;
-  }
-
-  function closeMobileMenu() {
-
-    isMobileMenuOpen = false;
-  }
-    function handleButtonClick() {
-        goto('/inscripcio');
+    function toggleMobileMenu() {
+        isMobileMenuOpen = !isMobileMenuOpen;
     }
- 
-  
+
+    function closeMobileMenu() {
+        isMobileMenuOpen = false;
+    }
+    function handleButtonClick() {
+        goto("/inscripcio");
+    }
 </script>
 
 <header>
     <div class="logo">
-        <img class="header-logo" src={logo} alt="header-logo"/>
-     </div>
-        <nav class="main-navbar">
+        <img class="header-logo" src={logo} alt="header-logo" />
+    </div>
+    <button class="mobile-menu-button" on:click={toggleMobileMenu}>☰</button>
+    <nav class="main-navbar">
         <ul>
-            <li class:current={$page.url.pathname === '/'}
-                aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/"}
+                aria-current={$page.url.pathname === "/" ? "page" : undefined}
+            >
                 <a href="/">Inici</a>
             </li>
-            <li class:current={$page.url.pathname === '/projecte'}
-                aria-current={$page.url.pathname === '/projecte' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/projecte"}
+                aria-current={$page.url.pathname === "/projecte"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/projecte">Projecte</a>
             </li>
-            <li class:current={$page.url.pathname === '/horari'}
-                aria-current={$page.url.pathname === '/horari' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/horari"}
+                aria-current={$page.url.pathname === "/horari"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/horari">Horari</a>
             </li>
-            <li class:current={$page.url.pathname === '/contratacions'}
-                aria-current={$page.url.pathname === '/contratacions' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/contratacions"}
+                aria-current={$page.url.pathname === "/contratacions"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/contratacions">Contratacions</a>
             </li>
-            <li class:current={$page.url.pathname === '/calendari'}
-                aria-current={$page.url.pathname === '/calendari' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/calendari"}
+                aria-current={$page.url.pathname === "/calendari"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/calendari">Calendari</a>
             </li>
             <button type="button" on:click={handleButtonClick}>
                 Inscripció i quotes
             </button>
-        </ul>
-        <button class="mobile-menu-button" on:click={toggleMobileMenu}>☰</button>
+        </ul>          
     </nav>
    
-</header>
-    
-    {#if isMobileMenuOpen}
-    
+
+
+{#if isMobileMenuOpen}
     <nav class="mobile-menu">
-          
         <ul>
-            <li class:current={$page.url.pathname === '/'}
-                aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/"}
+                aria-current={$page.url.pathname === "/" ? "page" : undefined}
+            >
                 <a href="/">Inici</a>
             </li>
-            <li class:current={$page.url.pathname === '/projecte'}
-                aria-current={$page.url.pathname === '/projecte' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/projecte"}
+                aria-current={$page.url.pathname === "/projecte"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/projecte">Projecte</a>
             </li>
-            <li class:current={$page.url.pathname === '/horaris'}
-                aria-current={$page.url.pathname === '/horaris' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/horaris"}
+                aria-current={$page.url.pathname === "/horaris"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/horaris">Horari</a>
             </li>
-            <li class:current={$page.url.pathname === '/contratacions'}
-                aria-current={$page.url.pathname === '/contratacions' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/contratacions"}
+                aria-current={$page.url.pathname === "/contratacions"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/contratacions">Contratacions</a>
             </li>
-            <li class:current={$page.url.pathname === '/calendari'}
-                aria-current={$page.url.pathname === '/calendari' ? 'page' : undefined}>
+            <li
+                class:current={$page.url.pathname === "/calendari"}
+                aria-current={$page.url.pathname === "/calendari"
+                    ? "page"
+                    : undefined}
+            >
                 <a href="/calendari">Calendari</a>
             </li>
             <button type="button" on:click={handleButtonClick}>
                 Inscripció i quotes
             </button>
         </ul>
-    </nav>
-        
-  {/if}
+           </nav>
+          
+{/if}
+</header>
 
-    
 <div class="img-fondo">
-    <img class="img-fondo" src={fondo} alt="img-fondo"/>
+    <img class="img-fondo" src={fondo} alt="img-fondo" />
 </div>
 
 <style>
@@ -102,15 +133,14 @@
         height: 20%;
         justify-content: space-between;
         align-items: center;
+       
     }
     .logo {
         margin: 2%;
-    
-     }
+    }
     .header-logo {
         height: 100%;
         max-width: 10%;
-     
     }
 
     ul {
@@ -120,16 +150,13 @@
         margin-right: 100px;
     }
 
-   
-
     ul li a {
         text-decoration: none;
         color: aliceblue;
-      
     }
 
     ul li.current a::before {
-        content: '';
+        content: "";
         position: absolute;
         bottom: -10px;
         left: 50%;
@@ -141,7 +168,6 @@
         border-bottom: 10px solid orangered;
     }
 
-   
     button {
         background-color: orangered;
         padding: 0.7em 1em;
@@ -156,36 +182,40 @@
         width: 100%;
         margin: 0;
         object-fit: cover;
-       
     }
     .mobile-menu-button {
-         display: none;
+        display: none;
     }
-
 
     @media screen and (max-width: 575px) {
+        
+       
 
-        ul{
+         header {
+        width: 100%;
         display: flex;
-        flex-direction: column;
+        background-color: black;
+        height: 5%;
         justify-content: space-between;
         align-items: center;
-        font-size:x-small;
-        gap: 20px;
-            
        
     }
-      
-    }    
-        
-        nav {
+    
+
+        ul {
             display: flex;
             flex-direction: column;
+            justify-content: space-between;
             align-items: center;
+            font-size: x-small;
+            gap: 20px;
+            background-color: rgb(43, 52, 52);
         }
+   
 
-    ul li.current a::before {
-        content: '';
+
+    .main-navbar ul li.current a::before {
+        content: "";
         position: absolute;
         bottom: -10px;
         left: 50%;
@@ -197,102 +227,119 @@
         border-bottom: 10px solid orangered;
     }
 
-    button  {
+
+    nav {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    ul li.current a::before {
+        content: "";
+        position: absolute;
+        bottom: -10px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 0;
+        height: 0;
+        border-left: 6px solid transparent;
+        border-right: 6px solid transparent;
+        border-bottom: 10px solid orangered;
+    }
+
+    button {
         background-color: orangered;
         padding: 0.2em 2em;
         border: none;
         color: aliceblue;
         border-radius: 5px;
         cursor: pointer;
-        font-size:xx-small;
+        font-size: xx-small;
     }
-    
+
     .mobile-menu-button {
         width: 6%;
         display: inline;
         padding: 1em 2em;
         align-items: center;
     }
-     
-    
-  
-  @media screen and (min-width: 768px) and (max-width: 991px) {
-    ul li a{
+
+    .mobile-menu {
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
         align-items: center;
-        background-color: transparent;
-        padding: 10px;
-      
-  }
-}
-   
-  
-  @media screen and (min-width: 1024px) {
-
-    header {
-        width: 100%;
-        display: flex;
-        background-color: black;
-        height: 20%;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .logo {
-   margin: 2%;
-    
-  }
-    .header-logo {
-        height: 100%;
-        max-width: 10%;
-       
-    }
-
-   
-    ul li a {
-        text-decoration: none;
-        color: aliceblue;
-        position: relative;
-    }
-
-    ul li.current a::before {
-        content: '';
         position: absolute;
-        bottom: -10px;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 0;
-        height: 0;
-        border-left: 6px solid transparent;
-        border-right: 6px solid transparent;
-        border-bottom: 10px solid orangered;
-    }
-
-  
-
-    button {
-        background-color: orangered;
-        padding: 0.8em 1em;
-        border: none;
-        color: aliceblue;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: small;
-       
-    }
-
-    .img-fondo {
+        top: 100%; 
+        left: 0;
         width: 100%;
-        margin: 0;
-        object-fit: cover;
-       
-    }
-    .mobile-menu-button {
-    display: none;
-  }
-      
-
+        background-color: black; 
+        padding: 20px; 
+        z-index: 1; 
     }
 
+    }
+    @media screen and (min-width: 768px) and (max-width: 991px) {
+        ul li a {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            background-color: transparent;
+            padding: 10px;
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+        header {
+            width: 100%;
+            display: flex;
+            background-color: black;
+            height: 20%;
+            justify-content: space-between;
+            align-items: center;
+        }
+        
+        .header-logo {
+            height: 100%;
+            max-width: 10%;
+        }
+
+        ul li a {
+            text-decoration: none;
+            color: aliceblue;
+            position: relative;
+        }
+
+        ul li.current a::before {
+            content: "";
+            position: absolute;
+            bottom: -10px;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 0;
+            height: 0;
+            border-left: 6px solid transparent;
+            border-right: 6px solid transparent;
+            border-bottom: 10px solid orangered;
+        }
+
+        button {
+            background-color: orangered;
+            padding: 0.8em 1em;
+            border: none;
+            color: aliceblue;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: small;
+        }
+
+        .img-fondo {
+            width: 100%;
+            margin: 0;
+            object-fit: cover;
+        }
+        .mobile-menu-button {
+            display: none;
+        }
+    }
 </style>
