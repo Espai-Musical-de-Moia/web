2be88595832ad3a88e248918ed38ad8071f76967
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-26 12:19:02 +0200

    calendario arreglado

--- src/routes/calendari/+page.svelte
+++ src/routes/calendari/+page.svelte
@@ -1,9 +1,10 @@
-<iframe src="https://calendar.google.com/calendar/embed?src=gp7oo28osimv8650r7n7v8gv9s%40group.calendar.google.com&ctz=Europe%2FMadrid" title="Calendari d'events"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23A79B8E&ctz=Europe%2FMadrid&showTitle=1&showNav=1&showPrint=0&showTabs=1&showCalendars=1&hl=ca&src=Z3A3b28yOG9zaW12ODY1MHI3bjd2OGd2OXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23A79B8E" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no" title="Calendari d'events"></iframe>
 
 <style>
-   iframe {
+    iframe {
         display: flex;
         width: 100%;
         height: 100vh;
     }
 </style>
+
